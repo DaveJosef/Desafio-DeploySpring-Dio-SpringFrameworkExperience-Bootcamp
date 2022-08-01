982f3823d80933b13a0d241a4a6a37ef245a767b
DaveJosef <josedaaaaavid@gmail.com>
2022-08-01 13:23:31 -0300

    Adiciona Swagger docs

--- src/main/java/one/digitalinnovation/parking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -1,11 +1,14 @@
 package one.digitalinnovation.parking.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("hello")
+@Api(tags = "Hello Controller")
 public class HelloController {
 
     @GetMapping("/")
