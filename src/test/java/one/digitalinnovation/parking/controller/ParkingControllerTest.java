80243652b7e57898089e5fc77823e3587621e51a
DaveJosef <josedaaaaavid@gmail.com>
2022-08-03 16:39:15 -0300

    Adiciona Testcontainers

--- src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
+++ src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ParkingControllerTest {
+public class ParkingControllerTest extends AbstractContainerBase {
 
     @LocalServerPort
     private int randomPort;
